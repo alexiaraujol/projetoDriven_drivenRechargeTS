919c3efd4320973fdd5734d7b5c156f2a22e2874
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-07-09 19:15:57 -0300

    consertando o database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,11 +1,16 @@
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
+
+console.log("URL do banco:", process.env.DATABASE_URL);
  
 const {Pool} = pg;
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default db;
