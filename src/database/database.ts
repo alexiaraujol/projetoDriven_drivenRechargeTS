24a250bde70f74007a59868899c337a468c20967
Alexia A. de Lima <alexiaalima96@gmail.com>
2025-07-09 20:15:08 -0300

    correções feitas

--- src/database/database.ts
+++ src/database/database.ts
@@ -7,7 +7,11 @@ console.log("URL do banco:", process.env.DATABASE_URL);
 const {Pool} = pg;
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 export default db;
